ba3bf4cfc2bf60dc6fd9d6e42419bc4cbad33bb6
Dalius Cvilikas <daliusc@wix.com>
2021-07-27 10:48:13 +0300

    Added App.js

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
 (() => {
-  const header = new Header(document.querySelector('.header'));
-  const services = new Services();
-  const sidebar = new Sidebar(
-    document.querySelector('.sidebar'),
-    document.querySelector('.event-btn'),
-    header.hamburger
-  );
-  const modal = new Modal(
-    document.querySelector('.event-btn'),
-    services.addEvent.bind(services)
-  );
-  const miniCalendar = new MiniCalendar(
-    document.querySelector('.mini-calendar'),
-    header.todayButton
-  );
-  const calendar = new Calendar(
-    document.querySelector('.calendar'),
-    header.todayButton,
-    header.monthText,
-    header.leftButton,
-    header.rightButton,
-    services.eventsList,
-    services.changeUpdateFunction.bind(services)
-  );
+  const app = new App();
 })();
